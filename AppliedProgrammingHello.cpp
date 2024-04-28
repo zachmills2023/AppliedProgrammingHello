2b5495b8720a356ca22872d6b47009ada1d2dcb3
zachmills135 <zachmills135@gmail.com>
2024-04-27 23:41:04 -0600

    Add project files.

---
+++ AppliedProgrammingHello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    string name;
+    cout << "Please enter your name: ";
+    cin >> name;
+    cout << "Hello " << name << endl;
+
+    return 0;
+}
+
